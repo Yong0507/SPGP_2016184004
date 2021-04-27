35a4ec85c38e506bafae831e8a7e7e5014f2ceae
YongJins <yongyong9689@gmail.com>
2021-04-27 18:39:32 +0900

    commit
    commit

---
+++ Term_Project/app/src/main/java/kr/ac/kpu/s2016184004/term_project/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.kpu.s2016184004.term_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
