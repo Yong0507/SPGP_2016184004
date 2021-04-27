e0512161a76f1e706e5cfb6cf5c4f64152fcd6fa
YongJins <yongyong9689@gmail.com>
2021-04-27 18:47:17 +0900

    Make FrameWork
    Make FrameWork

---
+++ Term_Project/app/src/main/java/kr/ac/kpu/s2016184004/term_project/framework/GameObject.java
@@ -0,0 +1,8 @@
+package kr.ac.kpu.s2016184004.term_project.framework;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
---
+++ Term_Project/app/src/main/java/kr/ac/kpu/s2016184004/term_project/framework/Recyclable.java
@@ -0,0 +1,5 @@
+package kr.ac.kpu.s2016184004.term_project.framework;
+
+public interface Recyclable {
+    public void recycle();
+}
